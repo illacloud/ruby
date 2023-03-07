a522d7e5901c97f111f6f9667b6e750e9836fc9f
smallSohoSolo <b3069741@gmail.com>
2023-03-07 11:20:29 +0800

    feat: blog style update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 (function () {
     pagination(true);
+    const share = document.querySelector('.social-media-share')
+    const shareList = document.querySelector('.share-list')
+    
+    share &&  share.addEventListener('click', (event) => {
+        shareList.style.display = 'block'
+        event.stopPropagation()
+    })
+    window.addEventListener('click', () => {
+        if(!shareList) return
+        if(shareList.style.display === 'block')  {
+            shareList.style.display = 'none'
+        }
+    })
 })();
